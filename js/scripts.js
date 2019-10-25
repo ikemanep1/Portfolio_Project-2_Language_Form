a7425f3317129571a626ce63c65997b17fcfff88
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2019-10-25 16:16:43 -0700

    edit js scripts so that they almost work

--- js/scripts.js
+++ js/scripts.js
@@ -11,58 +11,92 @@ $(document).ready(function(){
 
     if (interests === 1) {
       $("#resulta").show();
+      $("#resultb").hide();
+      $("resultc").hide();
     }
     if (interests === 2) {
       $("#resultb").show();
+      $("#resulta").hide();
+      $("resultc").hide();
     }
     if (interests === 3) {
       $("#resultc").show();
+      $("#resulta").hide();
+      $("resultb").hide();
     }
 
 
 
     if (scenario1 === 1) {
       $("#resulta").show();
+      $("#resultb").hide();
+      $("resultc").hide();
     }
     if (scenario1 === 2) {
       $("#resultb").show();
+      $("#resulta").hide();
+      $("resultc").hide();
     }
     if (scenario1 === 3) {
       $("#resultc").show();
+      $("#resulta").hide();
+      $("resultb").hide();
     }
 
 
     if (scenario2 === 1) {
       $("#resulta").show();
+      $("#resultb").hide();
+      $("resultc").hide();
     }
     if (scenario2 === 2) {
       $("#resultb").show();
+      $("#resultb").hide();
+      $("resultc").hide();
     }
     if (scenario2 === 3) {
       $("#resultc").show();
+      $("#resulta").hide();
+      $("resultb").hide();
     }
 
 
     if (scenario3 === 1) {
       $("#resulta").show();
+      $("#resultb").hide();
+      $("resultc").hide();
     }
     if (scenario3 === 2) {
       $("#resultb").show();
+      $("#resulta").hide();
+      $("resultc").hide();
     }
     if (scenario3 === 3) {
       $("#resultc").show();
+      $("#resulta").hide();
+      $("resultb").hide();
     }
 
 
     if (scenario4 === 1) {
       $("#resulta").show();
+      $("#resultb").hide();
+      $("resultc").hide();
     }
     if (scenario4 === 2) {
       $("#resultb").show();
+      $("#resulta").hide();
+      $("resultc").hide();
     }
     if (scenario4 === 3) {
       $("#resultc").show();
+      $("#resulta").hide();
+      $("resultb").hide();
     }
+
+    else {
+       alert('Thats enough, young man.');
+     }
 
   });
 });
