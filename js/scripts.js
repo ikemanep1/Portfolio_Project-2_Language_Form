e0bfe1f530b018ae58ea73dbe758c94c95acb20a
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2019-10-25 11:49:58 -0700

    fix problem where bootstrap is absent

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+var flavor = $("input:radio[name=q1a1]:checked").val();
+var flavor = $("input:radio[name=q1a2]:checked").val();
+var flavor = $("input:radio[name=q1a3]:checked").val();
+var flavor = $("input:radio[name=q2a1]:checked").val();
+var flavor = $("input:radio[name=q2a2]:checked").val();
+var flavor = $("input:radio[name=q2a3]:checked").val();
+var flavor = $("input:radio[name=q3a1]:checked").val();
+var flavor = $("input:radio[name=q3a2]:checked").val();
+var flavor = $("input:radio[name=q3a3]:checked").val();
+var flavor = $("input:radio[name=q4a1]:checked").val();
+var flavor = $("input:radio[name=q4a2]:checked").val();
+var flavor = $("input:radio[name=q4a3]:checked").val();
+var flavor = $("input:radio[name=q5a1]:checked").val();
+var flavor = $("input:radio[name=q5a2]:checked").val();
+var flavor = $("input:radio[name=q5a3]:checked").val();
