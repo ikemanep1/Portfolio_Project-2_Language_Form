1611133767c3385a17743d78f369cdafc3e46b10
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2019-10-25 15:59:32 -0700

    add initial js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,68 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  $("#userinterface").submit(function(event){
+    event.preventDefault();
+    var interests = parseInt($("select#interests").val());
+    var scenario1 = parseInt($("select#scenario1").val());
+    var scenario2 = parseInt($("select#scenario2").val());
+    var scenario3 = parseInt($("select#scenario3").val());
+    var scenario4 = parseInt($("select#scenario4").val());
+
+
+
+    if (interests === 1) {
+      $("#resulta").show();
+    }
+    if (interests === 2) {
+      $("#resultb").show();
+    }
+    if (interests === 3) {
+      $("#resultc").show();
+    }
+
+
+
+    if (scenario1 === 1) {
+      $("#resulta").show();
+    }
+    if (scenario1 === 2) {
+      $("#resultb").show();
+    }
+    if (scenario1 === 3) {
+      $("#resultc").show();
+    }
+
+
+    if (scenario2 === 1) {
+      $("#resulta").show();
+    }
+    if (scenario2 === 2) {
+      $("#resultb").show();
+    }
+    if (scenario2 === 3) {
+      $("#resultc").show();
+    }
+
+
+    if (scenario3 === 1) {
+      $("#resulta").show();
+    }
+    if (scenario3 === 2) {
+      $("#resultb").show();
+    }
+    if (scenario3 === 3) {
+      $("#resultc").show();
+    }
+
+
+    if (scenario4 === 1) {
+      $("#resulta").show();
+    }
+    if (scenario4 === 2) {
+      $("#resultb").show();
+    }
+    if (scenario4 === 3) {
+      $("#resultc").show();
+    }
+
+  });
+});
