219f64b8b0fe5e7a2d9ee8c8a26eacc53e450ff4
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2019-11-02 18:58:04 -0700

    clean up html, js to be more compact

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   $("#userinterface").submit(function(event){
     event.preventDefault();
 
-
 var interests = parseInt($("select#interests").val());
 var drivingQuestion = parseInt($("select#drivingquestion").val());
 var osQuestion = parseInt($("select#osquestion").val());
@@ -11,28 +10,21 @@ var goals = parseInt($("select#goals").val());
 
 var formResult = interests + drivingQuestion + osQuestion + energyDrinkQuestion + goals;
 
-  console.log(formResult)
-
     if (formResult <= 15 && formResult > 10) {
       $("#python").show();
       $("#swift").hide();
       $("#rust").hide();
     }
-
     else if (formResult <=10 && formResult > 5) {
-    $("#python").hide();
-    $("#swift").show();
-    $("#rust").hide();
-
+      $("#python").hide();
+      $("#swift").show();
+      $("#rust").hide();
     }
-
     else if (formResult < 6) {
-    $("#python").hide();
-    $("#swift").hide();
-    $("#rust").show();
-
+      $("#python").hide();
+      $("#swift").hide();
+      $("#rust").show();
     }
-
     else {
       alert("That's enough, citizen")
     }
